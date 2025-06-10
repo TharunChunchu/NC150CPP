12929f7eae482d04c49a8bfec3616aff35285483
tharun <ch.tharunkumar1@gmail.com>
2025-06-09 19:55:31 -0700

    group anagrams

---
+++ 49_group_anagrams.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+vector<vector<string>> groupAnagrams(vector<string> strs) {
+    vector<vector<string>> res;
+    unordered_map<string,vector<string>> ump;
+    for(auto i:strs){
+        string s=i;
+        sort(s.begin(),s.end());
+        if(ump.find(s)!=ump.end()){
+            ump[s].push_back(i);
+        }else{
+            ump[s]={i};
+        }
+    }
+    for(auto i:ump){
+        res.push_back(i.second);
+    }
+    return res;
+}
+int main(){
+    vector<string> v{"eat","tea","tan","ate","nat","bat"};
+    vector<vector<string>> res=groupAnagrams(v);
+    for(auto i:res){
+        for(auto j:i){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
