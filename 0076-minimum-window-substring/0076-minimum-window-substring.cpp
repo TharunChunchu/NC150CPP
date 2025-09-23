005ba88a07cf814ff3f6f8e57fc3bbb96b2a4f0b
Tharun chunchu <55255457+TharunChunchu@users.noreply.github.com>
2025-09-23 16:43:26 -0700

    Time: 787 ms (5.01%), Space: 11.7 MB (25.33%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int> ms,ss;
+        int counter=t.size();
+        for(auto i:t)ss[i]++;
+        bool flag=true;
+        int si=0,ei=s.size()-1;
+        int i=0;
+        int n=s.size();
+        int res=s.size();
+        for(int j=0;j<n;j++){
+            if(ms[s[j]]<ss[s[j]])counter--;
+            ms[s[j]]++;
+            while(counter==0 && i<n){
+                flag=false;
+                cout<<i<<" "<<j<<endl;
+                if(j-i+1<res){
+                    res=j-i+1;
+                    si=i;
+                    ei=j;
+                }
+
+                if(ms[s[i]]==ss[s[i]]){
+                    counter++;
+                }
+                ms[s[i]]--;
+
+                i++;
+            }
+
+        }
+        if(flag)return "";
+        return s.substr(si,ei-si+1);
+    }
+};
