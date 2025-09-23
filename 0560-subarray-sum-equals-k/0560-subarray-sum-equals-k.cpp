c027486cad869dcbb2549f252a63586d6edc93da
Tharun chunchu <55255457+TharunChunchu@users.noreply.github.com>
2025-09-23 13:34:13 -0700

    Time: 34 ms (91.19%), Space: 45.5 MB (55.22%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -5,13 +5,10 @@ public:
         ump[0]=1;
         int ps=0;
         int res=0;
-        for(int i=0;i<nums.size();i++){
-            ps+=nums[i];
-            if(ump.find(ps-k)!=ump.end()){
-                res+=ump[ps-k];
-            }
+        for(auto i:nums){
+            ps+=i;
+            if(ump.find(ps-k)!=ump.end())res+=ump[ps-k];
             ump[ps]++;
-
         }
         return res;
     }
