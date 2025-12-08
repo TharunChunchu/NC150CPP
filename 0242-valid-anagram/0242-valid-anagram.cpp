6e3318156bfec4be9450c207f05b8134af8b43e8
Tharun chunchu <55255457+TharunChunchu@users.noreply.github.com>
2025-12-08 15:22:15 +0530

    Time: 3 ms (50.28%), Space: 9.9 MB (25.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-      sort(s.begin(),s.end());
-      sort(t.begin(),t.end());
-      if(s==t){
+        unordered_map<char,int> ump;
+        for(auto ch:s){
+            ump[ch]++;
+        }
+        for(auto ch:t){
+            if(ump[ch]==0){
+                return false;
+            }
+            ump[ch]--;
+
+        }
+        for(auto it:ump){
+            if(it.second>=1){
+                return false;
+            }
+        }
         return true;
-      }  
-      return false;
     }
 };
