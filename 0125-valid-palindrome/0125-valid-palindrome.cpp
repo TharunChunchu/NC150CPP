42ac94c5996c6c8d459674a095da706838235ae1
Tharun chunchu <55255457+TharunChunchu@users.noreply.github.com>
2025-12-10 23:01:24 +0530

    Time: 2 ms (40.01%), Space: 10.3 MB (26.63%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-bool isPalindrome(string s1) {
-    string s="";
-    for(auto i:s1){
-        if(isalnum(i))s+=tolower(i);
+    bool isPalindrome(string s1) {
+        string s="";
+        for(auto ch:s1){
+            if(isalnum(ch))s+=tolower(ch);
+        }
+        int i=0, j=s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
-   int i=0;
-   int j=s.size()-1;
-   while(i<j){
-    if(s[i]!=s[j])return false;
-    i++;
-    j--;
-   }
-   return true;
-
-}
 };
