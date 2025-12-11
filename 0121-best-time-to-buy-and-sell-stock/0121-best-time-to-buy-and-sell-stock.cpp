2ba45e58dd0235af7ea722ea516b818b290823fc
Tharun chunchu <55255457+TharunChunchu@users.noreply.github.com>
2025-12-11 17:01:37 +0530

    Time: 0 ms (100%), Space: 97.3 MB (86.67%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int res=0;
+        int ans=0;
         int mn=prices[0];
         for(auto i:prices){
             mn=min(mn,i);
-            res=max(res,i-mn);
+            ans=max(ans,i-mn);
         }
-        return res;
+        return ans;
     }
 };
